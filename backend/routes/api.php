b29b4f88178244f1c472dce809097532a82d4e85
Jhenny Mercado <jmercadoa2001@gmail.com>
2025-08-04 18:29:23 -0400

    Archivo README actualizado

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,8 +4,11 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\MarcaController;
 use App\Http\Controllers\ProductoController;
+use App\Http\Controllers\AuthController;
 
  
 
 Route::apiResource('marcas', MarcaController::class);
 Route::apiResource('productos', ProductoController::class);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
