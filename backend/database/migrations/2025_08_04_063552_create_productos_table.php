dcc48194c47af3f0751aecceff7fcfbe1bf9ea90
Jhenny Mercado <jmercadoa2001@gmail.com>
2025-08-04 02:36:29 -0400

    Migracion de Tabla productos

--- backend/database/migrations/2025_08_04_063552_create_productos_table.php
+++ backend/database/migrations/2025_08_04_063552_create_productos_table.php
@@ -11,9 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('marcas', function (Blueprint $table) {
+        Schema::create('productos', function (Blueprint $table) {
             $table->id();
-            $table->string('nombre',50);
             $table->timestamps();
         });
     }
@@ -23,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('marcas');
+        Schema::dropIfExists('productos');
     }
 };
