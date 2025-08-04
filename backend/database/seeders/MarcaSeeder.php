9420488736167333c7be77adccedef4619a16530
Jhenny Mercado <jmercadoa2001@gmail.com>
2025-08-04 03:26:04 -0400

    Seeder para productos

--- backend/database/seeders/MarcaSeeder.php
+++ backend/database/seeders/MarcaSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
 
 class MarcaSeeder extends Seeder
 {
@@ -12,6 +14,18 @@ class MarcaSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // REGISTROS A LA TABLA MARCAS
+        DB::table('marcas')->insert([
+            [
+                'nombre' => 'Samsung',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nombre' => 'LG',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        ]);
     }
 }
