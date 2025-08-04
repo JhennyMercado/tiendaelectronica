08bfd3cde899bd01eae6d2faca3b0fe001ec6a23
Jhenny Mercado <jmercadoa2001@gmail.com>
2025-08-04 03:49:53 -0400

    Creacion de Modelo Marca

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -22,5 +22,6 @@ class DatabaseSeeder extends Seeder
             'email' => 'test@example.com',
         ]);*/
         $this->call(MarcaSeeder::class);
+        $this->call(ProductoSeeder::class);
     }
 }
--- backend/database/seeders/ProductoSeeder.php
+++ backend/database/seeders/ProductoSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
 
 class ProductoSeeder extends Seeder
 {
@@ -13,5 +15,21 @@ class ProductoSeeder extends Seeder
     public function run(): void
     {
         //
+        DB::table('productos')->insert([
+            [
+                'nombre_producto' => 'Televisor 50 pulgadas',
+                'precio' => '3500',
+                'marca_id' => 1, 
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nombre_producto' => 'Refrigerador 2 puertas',
+                'precio' => '4200',
+                'marca_id' => 2, 
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        ]);
     }
 }
